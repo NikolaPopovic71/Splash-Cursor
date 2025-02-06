1a07e18705d47efc9520ac37a381c0ba54a7d65c
NikolaPopovic71 <nikolapopovic71@gmail.com>
2025-02-06 16:09:34 +0100

    change vite file

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,5 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
-});
+    base: '/Splash-Cursor/',  // Must match GitHub repo name
+    build: {
+      outDir: 'dist',
+    }
+  });
+  
